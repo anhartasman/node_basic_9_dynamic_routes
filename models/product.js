5db324e2ccf76592a35fa58245dab66cb4af4ff2
Anhar Tasman <anhar_tasman@yahoo.com>
2022-01-03 17:50:27 +0700

    file bahan

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      // cb([]);
+      cb(JSON.parse(fileContent)); //<--terdapat error, akan dilanjutkan di tahap berikutnya
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      console.log(p);
+      fs.writeFile(p, JSON.stringify(products), (err) => {});
+    });
+  }
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
